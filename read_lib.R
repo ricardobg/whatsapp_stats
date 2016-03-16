546ef49470a31454307aff8eb82eff9617b09838
Thiago Lira <thiagolira@Thiagos-MacBook-Pro.local>
2016-03-16 11:00:18 -0300

    argumento com selecao de modelo e logica

--- read_lib.R
+++ read_lib.R
@@ -7,13 +7,21 @@ library(dplyr)
 
 ## Function to get Message vector
 ## Date of the message uses POSIXlt
-get_messages <- function(filename) {
-
+get_messages <- function(filename,model) {
+  
 	lines <- tolower(readLines(filename, warn=FALSE))
 	# Remove empty entries
 	lines <- lines[which(lines != '' & !is.na(lines))]
+	
+	if(model=="IOS"){
 	entry_regex <- '([0-9]{2}[/]){2}[0-9]{2} ([0-9]{2}[:]){3} .+'
-
+	}else if (model=="ANDROID")	{
+	 
+	entry_regex <- "([0-9][0-9]?[/])([0-9][0-9]?[/])[0-9]{2}[,] ([0-9]{2}[:][0-9]{2}) [-].+"
+	  
+	}
+	
+	
 	# Merge messages until no more messages without date/time
 	messages_without_time <- grep(entry_regex, lines, ignore.case=TRUE, invert=TRUE)
 	# Last index analysed
@@ -30,19 +38,32 @@ get_messages <- function(filename) {
 	lines <- lines[-messages_without_time]
 
 	#Now we have all lines with date
+	
+	if(model=="IOS"){
 	messages_regex <- '([0-9]{2}[/]){2}[0-9]{2} ([0-9]{2}[:]){3} .+[:].+'
+	}else if(model=="ANDROID"){
+	messages_regex <- "([0-9][0-9]?[/])([0-9][0-9]?[/])[0-9]{2}[,] ([0-9]{2}[:][0-9]{2}) [-].+"
+	
+	}
+	
 	temp_messages <- lines[grep(messages_regex, lines)]
-
+	
+	
+	if(model=="IOS"){
 	date_regex <- '([0-9]{2}[/]){2}[0-9]{2} ([0-9]{2}[:]){3} '
 	user_regex <- '^[^:]+[:] '
 	msg_regex  <- '.+'
-
+	}else if (model=="ANDROID"){
+	  date_regex <- "([0-9][0-9]?[/])([0-9][0-9]?[/])[0-9]{2}[,] ([0-9]{2}[:][0-9]{2})"
+	  user_regex <- '^[^-]+[:] '
+	  msg_regex  <- '.+'
+  }
 	dates <- regmatches(temp_messages, regexpr(date_regex, temp_messages)) 
 	temp_messages <- sub(date_regex, '', temp_messages)
 	users <- sub(": ", "", regmatches(temp_messages, regexpr(user_regex, temp_messages)), fixed=TRUE)
 	msgs <- sub(user_regex, '', temp_messages)
 
-	res <- data.frame(DATE=as.POSIXlt(dates, "%d/%m/%y %H:%M:%S: "), USER=users, MESSAGE=msgs, MESSAGE_LENGTH=sapply(msgs, 
+	res <- data.frame( USER=users, MESSAGE=msgs, MESSAGE_LENGTH=sapply(msgs, 
 	               function(m) { if (m == '<\U200Eimage omitted>') 0 else nchar(m) }))
 	tbl_df(res)
 }
