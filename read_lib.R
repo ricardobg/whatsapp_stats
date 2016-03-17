fda4b32d2661dbfd5d8bc352f94a867d59deed9a
Ricardo Boccoli Gallego <ricardo.gallego93@gmail.com>
2016-03-16 23:36:01 -0300

    refactored and automatic detect

--- read_lib.R
+++ read_lib.R
@@ -1,109 +1,78 @@
 library(dplyr)
 
-## Message object has the 
-
-# Message types
-
-
-## Function to get Message vector
+## Function to load Message vector
 ## Date of the message uses POSIXlt
-get_messages <- function(filename,model) {
+load_messages <- function(filename, model = 'automatic') {
+  # Table with config (regex and others)
+  config <- data.frame(date_regex=character(2), date_message_separator=character(2), row.names = c("android", "ios"), stringsAsFactors = FALSE)
   
-	lines <- tolower(readLines(filename, warn=FALSE,encoding='utf-8'))
+  # Setting android config
+  config['android', 'date_regex'] <- '([0-9][0-9]?[/]){2}[0-9]{2}([0-9]{2})?[,] ([0-9]{2}[:][0-9]{2})'
+  config['android', 'date_message_separator'] <- ' -'
+  
+  # Setting ios config
+  config['ios', 'date_regex'] <- '([0-9]{2}[/]){2}[0-9]{2} ([0-9]{2}[:]){2}[0-9]{2}'
+  config['ios', 'date_message_separator'] <- ':'
+  
+  #Put ^ as start
+  config[, 'date_regex'] = paste('^', config[, 'date_regex'] ,sep='')  
+	
+	
+	# Load lines
+	lines <- tolower(readLines(filename, warn=FALSE, encoding='utf-8'))
 	# Remove empty entries
 	lines <- lines[which(lines != '' & !is.na(lines))]
 	
-	if(model=="IOS"){
-	entry_regex <- '([0-9]{2}[/]){2}[0-9]{2} ([0-9]{2}[:]){3} .+'
-	}else if (model=="ANDROID")	{
-	 
-	entry_regex <- "([0-9][0-9]?[/])([0-9][0-9]?[/])[0-9]{2}[0-9]?[0-9]?[,] ([0-9]{2}[:][0-9]{2}) [-].+"
-	  
+	
+	# Automatic
+	if (model == 'automatic') {
+	  for (m in row.names(config)) {
+	    test_regex <- paste(config[m, 'date_regex'], config[m, 'date_message_separator'], ' .+$', sep = '')
+	    test_messages <- grep(test_regex, lines)
+	    # if this is the one, breaks
+	    if (length(test_messages) > 0) {
+	      model <- m
+	      break
+	    }
+	  }
 	}
+	
+	# Make regex to recognize a line
+	entry_regex <- paste(config[model, 'date_regex'], config[model, 'date_message_separator'], ' .+$', sep = '')
+	# Make regex to recognize MESSAGES
+	message_regex <- paste(config[model, 'date_regex'], config[model, 'date_message_separator'], ' [^:]+[:] .+$', sep = '')
 	
 	
 	# Merge messages until no more messages without date/time
-	messages_without_time <- grep(entry_regex, lines, ignore.case=TRUE, invert=TRUE)
+	messages_without_time <- grep(entry_regex, lines, ignore.case = TRUE, invert = TRUE)
 	# Last index analysed
 	index_last <- -1
 	# Destination
 	dest_last <- 0
 	if(length(messages_without_time)!=0 ){
-	for (i in messages_without_time) {
-		if (index_last != i - 1) {
-			dest_last <- i - 1
-		}
-		lines[dest_last] <- paste(lines[dest_last], lines[i])
-		index_last <- i
-	}
-	lines <- lines[-messages_without_time]
-}
-	#Now we have all lines with date
-	
-	if(model=="IOS"){
-	messages_regex <- '([0-9]{2}[/]){2}[0-9]{2} ([0-9]{2}[:]){3} .+[:].+'
-	}else if(model=="ANDROID"){
-	messages_regex <- "([0-9][0-9]?[/])([0-9][0-9]?[/])[0-9]{2}[0-9]?[0-9]?[,] ([0-9]{2}[:][0-9]{2}) [-].+"
-	
-	}
-	
-	temp_messages <- lines[grep(messages_regex, lines)]
-	
-	
-	
-	
-	
-	
-	if(model=="IOS"){
-	date_regex <- '([0-9]{2}[/]){2}[0-9]{2} ([0-9]{2}[:]){3} '
-	user_regex <- '^[^:]+[:] '
-	user_regex2 <- '[^:]+[:] '
-	msg_regex  <- '.+'
-	}else if (model=="ANDROID"){
-	  date_regex <- "([0-9][0-9]?[/])([0-9][0-9]?[/])[0-9]{2}[0-9]?[0-9]?[,] ([0-9]{2}[:][0-9]{2})"
-	  #user_regex <- '^[^-].+[:] '
-	  #user_regex<-'^ [-] ([[a-z]][ ]?)+[:]'
-	  #user_regex2<-' [-] ([a-z][ ]?)+[:]'
-	  user_regex <- '^[^:]+[:] '
-	  user_regex2 <- '[^:]+[:] '
-	  msg_regex  <- '.+'
-	}
-	
+  	for (i in messages_without_time) {
+  		if (index_last != i - 1) {
+  			dest_last <- i - 1
+  		}
+  		lines[dest_last] <- paste(lines[dest_last], lines[i])
+  		index_last <- i
+  	}
+  	lines <- lines[-messages_without_time]
+  }
+	# Now we have all lines with date
+
 	##Remove entries with events (such as create group) instead of messages
 	##TODO: Create list of events BROCOLLESSSSSSS
-	##user_regex2 eh a regex do user sem determinar o comeco de string
-	temp_messages <- temp_messages[grep(user_regex2,temp_messages)]
+	messages <- lines[grep(message_regex, lines)]
+	events <- lines[grep(message_regex, lines, invert = TRUE)]
 
-	
-	
-	if(model=="IOS"){
-	dates <- regmatches(temp_messages, regexpr(date_regex, temp_messages)) 
-	temp_messages <- sub(date_regex, '', temp_messages)
-	users <- sub(": ", "", regmatches(temp_messages, regexpr(user_regex, temp_messages)), fixed=TRUE)
-	msgs <- sub(user_regex, '', temp_messages)
+	# Set column date
+	column_date <- regmatches(messages, regexpr(config[model, 'date_regex'] , messages))
+	temp_messages <- sub(paste('^', config[model, 'date_regex'], config[model, 'date_message_separator'], ' ' ,sep=''), '', messages)
+	column_users <- sub('[:] $', '', regmatches(temp_messages, regexpr('^[^:]+[:] ', temp_messages)))
+	temp_messages <- sub(paste('^[^:]+[:] ' ,sep=''), '', temp_messages)
+	column_messages <- temp_messages
 
-	}else if(model=="ANDROID"){
-	  dates <- regmatches(temp_messages, regexpr(date_regex, temp_messages)) 
-	  temp_messages <- sub(date_regex, '', temp_messages)
-	  
-	  users <- sub(": ", "", regmatches(temp_messages, regexpr(user_regex, temp_messages)), fixed=TRUE)
-	 
-	  users <- sub(" - ","",users)
-	 
-	  msgs <- sub(user_regex, '', temp_messages)
-	  
-	}
-	
-	if(model=="IOS"){
-	
-	res <- data.frame(DATE=as.POSIXct(dates, "%d/%m/%y %H:%M:%S ",tz=Sys.timezone()), USER=users, MESSAGE=msgs, MESSAGE_LENGTH=sapply(msgs, 
-	               function(m) { if (m == '<\U200Eimage omitted>') 0 else nchar(m) }))
-	
-	} else if (model== "ANDROID" ){
-	  res <- data.frame(DATE=as.POSIXct(dates, "%m/%d/%y, %H:%M",tz=Sys.timezone()), USER=users, MESSAGE=msgs, MESSAGE_LENGTH=sapply(msgs, 
-	                                                                                                              function(m) { if (m == '<\U200Eimage omitted>') 0 else nchar(m) }))
-	}
-	
-	
-	tbl_df(res)
+	tbl_df(data.frame(DATE=column_date, USER=column_users, MESSAGE=column_messages))
+
 }
