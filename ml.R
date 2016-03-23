98982f0daf4a3f857c8ac954c2e77c07fc015c2b
Ricardo Boccoli Gallego <ricardo.gallego93@gmail.com>
2016-03-23 15:05:46 -0300

    Started machine learning for messages

---
+++ ml.R
@@ -0,0 +1,9 @@
+# Machine learning
+
+learn.learn_from_messages <- function (messages) {
+    #TODO: learn from messages and return structure using logistic regression
+}
+
+lear.predict <- function (learn_structure, message) {
+    #TODO: Predict who sent the message
+}
