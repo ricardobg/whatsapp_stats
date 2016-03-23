8dd4f68a70771ff41607c0e7ea216468eb09740f
ThiagoLira <thlira15@gmail.com>
2016-03-23 18:14:16 -0300

    funcao custo adicionada

--- ml.R
+++ ml.R
@@ -7,3 +7,20 @@ learn.learn_from_messages <- function (messages) {
 lear.predict <- function (learn_structure, message) {
     #TODO: Predict who sent the message
 }
+
+
+costFunction <- function(theta,X,y ){
+  
+  
+  resultados <- sigmoid(X%*%theta);
+  m <- size(y)[1]
+  
+  
+  J <- (1/m) *  (-y*log(resultados)-(1.-y)*log(1.-resultados));
+  
+  return (sum(J))##retornar custo
+  
+}
+
+
+
